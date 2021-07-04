0e0cbc55c2a7b7fff7e60b1d1948e829e71f2557
Kei Mizubuchi <41995658+Kmzbch@users.noreply.github.com>
2021-07-04 11:25:58 -0400

    tried flask_login as backend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,request,redirect
+
+from flask import Flask,render_template,request,redirect, jsonify
 from flask_login import login_required, current_user, login_user, logout_user
 from models import UserModel,db,login
 
@@ -7,7 +8,6 @@ app.secret_key = 'xyz'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db.init_app(app)
 login.init_app(app)
@@ -22,46 +22,51 @@ def create_all():
 def blog():
     return render_template('blog.html')
 
-
-@app.route('/login', methods = ['POST', 'GET'])
+@app.route('/login', methods=['POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect('/blogs')
-    
-    if request.method == 'POST':
-        email = request.form['email']
-        user = UserModel.query.filter_by(email = email).first()
-        if user is not None and user.check_password(request.form['password']):
-            login_user(user)
-            return redirect('/blogs')
-    
-    return render_template('login.html')
+    json_data = request.json
+    print(json_data)
+    email = json_data['email']
+    password = json_data['password']
+    user = UserModel.query.filter_by(email = email).first()
+    if user is not None and user.check_password(password):
+        login_user(user)
+        # session['logged_in'] = True
+        status = True
+    else:
+        status = False
+    return jsonify({'result': status})
 
-
-@app.route('/register', methods=['POST', 'GET'])
+@app.route('/register', methods=['POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect('/blogs')
-    
-    if request.method == 'POST':
-        email = request.form['email']
-        username = request.form['username']
-        password = request.form['password']
+    json_data = request.json
 
-        if UserModel.query.filter_by(email=email).first():
-            return ('Email already Present')
-            
-        user = UserModel(email=email, username=username)
-        user.set_password(password)
+    email = json_data['email']
+    username = json_data['username']
+    password = json_data['password']
+
+    if UserModel.query.filter_by(email=email).first():
+        return ('Email already Present')
+
+    user = UserModel(email=email, username=username)
+    user.set_password(password)
+
+    try:
         db.session.add(user)
         db.session.commit()
-        return redirect('/login')
-    return render_template('register.html')
+        status = 'success'
+    except:
+        status = 'this user is already registered'
+
+    db.session.close()
+
+    return jsonify({'result': status})
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect('/blogs')
+    session.pop('logged_in', None)
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
